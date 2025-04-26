b37d2a9400c4e8ea7b4edda077421a13281ca0c4
Razzemmatazz <emma@ursa.codes>
2025-04-26 13:17:39 -0500

    enabled export for static site

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   transpilePackages: ["three"],
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
